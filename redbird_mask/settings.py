fbce05306b8342ad4cbd43d5657e91af5bf9bffa
xi he <xihe@xis-MacBook-Pro.local>
2022-07-31 15:53:36 +0800

    update 2022-7-31v1

--- redbird_mask/settings.py
+++ redbird_mask/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_9rik9)!lyde#_o5z5stiiq7$!qddb7+nq^c*pjh--rzsu^ak9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
